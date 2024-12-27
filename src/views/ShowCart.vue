19cbce28ed124060d33c2f7a5d5bd882be74ed7c
Kristina Palmquist <kristina.palmquist@gmail.com>
2024-12-27 16:56:56 +0100

    Updates the ShowCart.vue file

--- src/views/ShowCart.vue
+++ src/views/ShowCart.vue
@@ -15,16 +15,13 @@ let productImagePaths = [];
 const resolveImagePath = (product) => {
   if (!product || !product.image) return '';
   try {
-    const imagePath =
-      // `../../` +
-      product.image;
+    const imagePath = `src/` + product.image;
     return new URL(imagePath, import.meta.url).href;
   } catch (e) {
     console.error(`Image not found for product: ${product.name}`);
     return '';
   }
 };
-// https://retroretreat.netlify.app/assets/images/footwear/star.jpg
 
 const formatCurrency = (value) => {
   return new Intl.NumberFormat('en-US', {
